a6f4b5dce0a4de11fe23da8932398a01101dd05a
AndreyFSouza <andreyfsouzaa@gmail.com>
2024-04-24 15:06:01 -0300

    update

--- FilaDeEspera/src/main/java/Package/Sistema.java
+++ FilaDeEspera/src/main/java/Package/Sistema.java
@@ -17,14 +17,12 @@ public class Sistema {
 
     @SuppressWarnings("unchecked")
     public Sistema() {
-        // Inicializando as filas de prioridades
         filasPrioridades = (Queue<Object>[]) new Queue[5];
         for (int i = 0; i < 5; i++) {
             filasPrioridades[i] = new ArrayDeque<>();
         }
     }
 
-    // Adiciona um elemento com uma determinada prioridade
     public void enfileirar(Object elemento, int prioridade) {
         if (prioridade >= 0 && prioridade < 5) {
             filasPrioridades[prioridade].add(elemento);
@@ -33,21 +31,16 @@ public class Sistema {
         }
     }
 
-    // Remove e retorna o elemento com maior prioridade
 
     public void desenfileirar() {
     for (int i = 0; i < 5; i++) {
-        // Verifica se a fila de prioridade não está vazia
         if (!filasPrioridades[i].isEmpty()) {
-            filasPrioridades[i].poll(); // Remove o elemento da frente da fila
-            return; // Retorna imediatamente após remover um elemento
+            filasPrioridades[i].poll(); 
+            return; 
         }
     }
-    // Se todas as filas de prioridade estiverem vazias, não faz nada
 }
    
-
-    
     public int contarElementos() {
     int totalElementos = 0;
     for (Queue<Object> fila : filasPrioridades) {
@@ -61,16 +54,12 @@ public class Sistema {
     for (int i = 0; i < 5; i++) {
         int size = filasPrioridades[i].size();
         if (indice< count + size) {
-            // Encontramos a fila que contém o índice global
-            // Calculamos o índice local dentro dessa fila
             int indiceLocal = indice - count;
-            // Converte a fila de prioridade para uma lista para acessar pelo índice
             Object[] elementos = filasPrioridades[i].toArray();
             return elementos[indiceLocal];
         }
         count += size;
     }
-    System.out.println("Índice global inválido.");
     return null;
 }
 }    
