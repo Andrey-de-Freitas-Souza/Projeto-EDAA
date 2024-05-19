26c53cb17e6961456c42120567c0e3e71014843b
AndreyFSouza <andreyfsouzaa@gmail.com>
2024-05-19 01:24:00 -0300

    update

--- FilaDeEspera/src/main/java/Package/Sistema.java
+++ FilaDeEspera/src/main/java/Package/Sistema.java
@@ -1,10 +1,12 @@
 package Package;
 import java.util.Comparator;
 
-public class Sistema  implements Comparator<Paciente>{
+public class Sistema implements Comparator<Paciente>{
     @Override
     public int compare(Paciente o1, Paciente o2) {
          return o1.getPosicaoFila() < o2.getPosicaoFila() ? 1 : -1;
     }
 }    
+
+
 
